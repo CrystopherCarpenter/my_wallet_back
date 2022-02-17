a4e2c075f080b4c0238f8badc03f6ec12c5f1a25
Crystopher Carpenter <crystopher_stanley@live.com>
2022-02-17 18:13:14 -0300

    fix: searching for problems

--- index.js
+++ index.js
@@ -54,12 +54,17 @@ app.post('/signup', async (req, res) => {
 	user.name = stripHtml(user.name).result.trim();
 
 	const passwordHash = bcrypt.hashSync(user.password, 10);
+	const envio = [
+		{ passwordHash },
+		{ ...user, [password]: passwordHash },
+		{ ...user, password: passwordHash },
+	];
 
 	try {
 		await db
 			.collection('users')
-			.insertOne({ ...user, password: passwordHash });
-		res.sendStatus(201);
+			.insertOne({ ...user, [password]: passwordHash });
+		res.send(envio).status(201);
 	} catch {
 		res.sendStatus(500);
 	}
@@ -69,6 +74,8 @@ app.post('/signin', async (req, res) => {
 	const { email, password } = req.body;
 
 	const user = await db.collection('users').findOne({ email });
+
+	console.log(user.password);
 
 	if (user && bcrypt.compareSync(password, user.password)) {
 		const token = uuid();
@@ -82,7 +89,7 @@ app.post('/signin', async (req, res) => {
 			res.sendStatus(500);
 		}
 	} else {
-		res.sendStatus(401);
+		res.send(user.password).status(401);
 	}
 });
 
@@ -91,13 +98,13 @@ app.get('/mywallet', async (req, res) => {
 	const token = authorization?.replace('Bearer ', '');
 
 	if (!token) {
-		return res.sendStatus(401);
+		return res.send('token').status(401);
 	}
 
 	const session = await db.collection('sessions').findOne({ token });
 
 	if (!session) {
-		return res.sendStatus(401);
+		return res.send(session).status(401);
 	}
 
 	const userRecords = await db
