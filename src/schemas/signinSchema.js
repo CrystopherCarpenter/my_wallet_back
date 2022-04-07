68a951d114283b4bd1e8083b6bec88aa9c1efc6f
Crystopher Carpenter <crystopher.stanley.carpenter@gmail.com>
2022-04-07 13:28:06 -0300

    fix: fix schemas

--- src/schemas/signinSchema.js
+++ src/schemas/signinSchema.js
@@ -3,9 +3,12 @@ import joi from 'joi';
 const signinSchema = joi.object({
     password: joi
         .string()
-        .pattern(/^[a-zA-Z0-9]{3,30}$/)
+        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
         .required(),
-    email: joi.string().email().required(),
+    email: joi
+        .string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+        .required(),
 });
 
 export default signinSchema;
--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -4,9 +4,12 @@ const userSchema = joi.object({
     name: joi.string().alphanum().min(3).max(30).required(),
     password: joi
         .string()
-        .pattern(/^[a-zA-Z0-9]{3,30}$/)
+        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
         .required(),
-    email: joi.string().email().required(),
+    email: joi
+        .string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+        .required(),
 });
 
 export default userSchema;
