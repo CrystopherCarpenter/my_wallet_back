67655113d38cfd2d5b51c86efa4097ff540c495d
Crystopher Carpenter <crystopher.stanley.carpenter@gmail.com>
2022-04-07 13:30:10 -0300

    test

--- src/middleware/validateSchema.js
+++ src/middleware/validateSchema.js
@@ -2,6 +2,7 @@ export default function validateSchema(schema) {
     return (req, res, next) => {
         const validation = schema.validate(req.body);
         if (validation.error) {
+            console.log(validation.error);
             return res.sendStatus(400);
         }
 
