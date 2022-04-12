44108522ad8baf94e6ea83a8beffb922beaf70d6
Crystopher Carpenter <crystopher.stanley.carpenter@gmail.com>
2022-04-12 13:04:19 -0300

    Prepare for deploy

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ app.use(cors());
 app.use(json());
 app.use(router);
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on ${process.env.PORT}`);
 });
